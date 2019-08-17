748c3ab8d40ba5dd41e51d7da9f7d287f2afec85
Nathan Glover <nathan@glovers.id.au>
2019-08-17 21:36:53 +0800

    added dynamo integration

--- image-handler/handler.py
+++ image-handler/handler.py
@@ -1,11 +1,13 @@
 import base64
 import boto3
 import os
-import time
 import json
+import uuid
+import time
 
 bucket_name = os.environ['BUCKET_NAME']
 queue_name = os.environ['QUEUE_NAME']
+dynamo_table = os.environ['DYNAMO_TABLE']
 
 
 def selfie(event, context):
@@ -18,8 +20,10 @@ def selfie(event, context):
 
     # Create S3 client and store image
     s3 = boto3.client('s3')
-    file_name = 'archive/image-'+time.strftime("%Y%m%d-%H%M%S")+'.jpg'
-    response = s3.put_object(Body=image, Bucket=bucket_name, Key=file_name)
+    folder_name = 'incoming'
+    file_name = str(uuid.uuid1())+'.jpg'
+    file_path = folder_name + '/' + file_name
+    response = s3.put_object(Body=image, Bucket=bucket_name, Key=file_path)
 
     # Create SQS client and send data for processing
     sqs = boto3.client('sqs')
@@ -29,13 +33,26 @@ def selfie(event, context):
     # Construct message
     message = {
         "bucket_name": bucket_name,
-        "bucket_key": file_name,
+        "bucket_key": file_path,
+        "file_name": file_name,
         "email": email,
         "crop": crop
     }
 
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(dynamo_table)
+    response = table.put_item(
+        Item={
+            'timestamp': str(time.time()),
+            'email': email,
+            'bucket': bucket_name,
+            'key': file_name
+        }
+    )
+
     # Send message to SQS queue
-    response = sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message))
+    response = sqs.send_message(
+        QueueUrl=queue_url, MessageBody=json.dumps(message))
 
     response = {
         "headers": {
