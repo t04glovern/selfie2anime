7fce60e5659b5d412d02d86f8c5a6e97a8558651
Nathan Glover <nathan@glovers.id.au>
2019-08-29 23:23:17 +0800

    fixed html

--- components/302-redirect/index.js
+++ components/302-redirect/index.js
@@ -2,45 +2,36 @@
 
 const path = require('path')
 
-exports.handler = (event, context, callback) => {
-    //get request object
-    const { request } = event.Records[0].cf
-
-    const url = request.uri;
-
-    //we need to determine if this request has an extension.
-    const extension = path.extname(url);
-
-    //path.extname returns an empty string when there's no extension.
-    //if there is an extension on this request, continue without doing anything!
-    if(extension && extension.length > 0){
-        return callback(null, request);
-    }
-    
-    //there is no extension, so that means we want to add a trailing slash to the url.
-    //let's check if the last character is a slash.
-    const last_character = url.slice(-1);
-    
-    //if there is already a trailing slash, return.
-    if(last_character === "/"){
-        return callback(null, request);
-    }
-
-    //add a trailing slash.    
-    const new_url = `${url}/`;
-
-
-    //create HTTP redirect...
-    const redirect = {
+function redirect301(url) {
+    return {
         status: '301',
         statusDescription: 'Moved Permanently',
         headers: {
             location: [{
                 key: 'Location',
-                value: new_url,
+                value: url,
             }],
         },
     };
-    
-    return callback(null, redirect);
+}
+
+exports.handler = (event, context, callback) => {
+    const {
+        request
+    } = event.Records[0].cf
+    const url = request.uri;
+
+    if (url.endsWith('/')) {
+        return callback(null, request);
+    }
+
+    if (url.endsWith('/index.html')) {
+        return callback(null, redirect301(url.replace('/index.html', '/')));
+    }
+
+    if (path.extname(url).length > 0) {
+        return callback(null, request);
+    }
+
+    return callback(null, redirect301(url + '/'));
 };
